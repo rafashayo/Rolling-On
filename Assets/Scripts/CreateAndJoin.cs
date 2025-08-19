544650829d58f022e31248c7bf915b3bf4e5a385
49380211 <49380211@est.ort.edu.ar>
2025-08-19 09:06:00 -0300

    dsads

---
+++ Assets/Scripts/CreateAndJoin.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+
+public class CreateAndJoin : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField input_Create;
+    public TMP_InputField input_Join;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(input_Create.text);
+
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(input_Join.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("GamePlay");
+    }
+}

ec420515dc100b7a2958d5151347b8d2e4a013de
49380211 <49380211@est.ort.edu.ar>
2025-08-31 17:07:12 -0300

    cambio

---
+++ Assets/Scripts/IrAPartida.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IrAPartida : MonoBehaviour
+{
+  
+
+    public void IrAEscena()
+    {
+        SceneManager.LoadScene("Joinearse");
+    }
+
+}

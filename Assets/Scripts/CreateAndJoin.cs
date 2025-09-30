db0f5e1ce51227e0de4812a70a8c350833366f18
49380211 <49380211@est.ort.edu.ar>
2025-09-30 08:59:29 -0300

    spawnea en terreno perf

--- Assets/Scripts/CreateAndJoin.cs
+++ Assets/Scripts/CreateAndJoin.cs
@@ -28,7 +28,7 @@ public class CreateAndJoin : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("Multiplayer");
+        PhotonNetwork.LoadLevel("Terreno");
     }
 
 

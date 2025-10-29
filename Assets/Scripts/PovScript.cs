8de214879042f8d67b47fa07c38aad9626b539bc
rafashayo <49433616@est.ort.edu.ar>
2025-10-29 09:57:23 -0300

    camara hecha

--- Assets/Scripts/PovScript.cs
+++ Assets/Scripts/PovScript.cs
@@ -1,16 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PovScript1 : MonoBehaviour
+public class PovScript : MonoBehaviour
 {
+    private float x;
+    private float y;
+    private float sensitivity = -1f;
+    private Vector3 rotate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        y = Input.GetAxis("Mouse X"); 
+        x = Input.GetAxis("Mouse Y");
+        rotate = new Vector3(x, y * sensitivity, 0);
+        transform.eulerAngles = transform.eulerAngles - rotate; 
+
     }
 }
